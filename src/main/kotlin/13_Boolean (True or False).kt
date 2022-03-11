15ad106428f9d8b5b893be241513ddb55e0a135b
Eka Juliawati <ekajuliawati63@gmail.com>
2022-03-11 18:58:45 +0700

    Percobaan Boolean ( True or False )

---
+++ src/main/kotlin/13_Boolean (True or False).kt
@@ -0,0 +1,11 @@
+// Boolean True or False
+fun main(args: Array<String>) {
+//Tipe data Boolean memiliki nilai true atau false. Nilai 0 dan 1 tidak berlaku untuk menunjuk nilai pada sebuah variabel bertipe Boolean
+    var varBool : Boolean = true
+    if(varBool)
+        println("varBool bernilai " + varBool)
+//    var varInt : Int = 1 // Nilai 0 dan 1 tidak berlaku untuk menunjuk nilai pada sebuah variabel bertipe Boolean
+    var varInt : Boolean = false
+    if(varInt)
+        println("varInt bernilai " + varInt)
+}
