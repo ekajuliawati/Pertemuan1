fdc098ba806ed495729d0cbe3d9a775c8cfdc880
Eka Juliawati <ekajuliawati63@gmail.com>
2022-03-11 16:11:21 +0700

    percobaan blok dan komentar

---
+++ src/main/kotlin/3_Blok dan komentar.kt
@@ -0,0 +1,11 @@
+fun main() {
+    println("Hello, Saya Baru Belajar Kotlin Nich")
+    //komentar 1 baris
+    /*
+    ini contoh komentar 1 baris
+     */
+    //komentar multi baris
+    /**
+     * contoh komentar multi baris
+     */
+}
