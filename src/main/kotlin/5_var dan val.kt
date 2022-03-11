c8ad3cdc8d104bdb08fcf8f5a9e3ef0d75c9b1d7
Eka Juliawati <ekajuliawati63@gmail.com>
2022-03-11 16:18:28 +0700

    percobaan var dan val

---
+++ src/main/kotlin/5_var dan val.kt
@@ -0,0 +1,14 @@
+fun main() {
+    // var yaitu nilai yang bisa di ubah selama di eksekusi
+    var varVariabel: Int
+    varVariabel = 56 // memberi nilai pertama pada varVariabel
+    println("Pemberian nilai pertama pada varVariabel = " + varVariabel)
+    varVariabel = 78 // memberi nilai kedua pada varVariabel
+    println("Pemberian nilai kedua pada varVariabel = " + varVariabel)
+
+    // val bernilai tetap
+    val phi = 3.14
+    phi = 2.5
+    // Ketika kita merubah nilai variabel val phi untuk kedua kalinya maka akan terdapat pesan
+    //kesalahan agar kita menggunakan var variabel.
+}
