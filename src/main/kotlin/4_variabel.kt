8c3bcea73e2dfe6570d59e4748b8dcf02e79cbd8
Eka Juliawati <ekajuliawati63@gmail.com>
2022-03-11 16:12:08 +0700

    percobaan Variabel statis dan dinamis

---
+++ src/main/kotlin/4_variabel.kt
@@ -0,0 +1,13 @@
+fun main() {
+    // variabel statis dan dinamis
+    var dinamis = "Hero"
+    var statis = 2.0
+    println(statis)
+
+     // variabel dinamis tidak memerlukan adanya deklarasi tipe data pada sebuah variabel
+    var dynamicVar = 2
+    println ("Contoh dynamic variabel = " + dynamicVar)
+    // variabel statis di harus kan untuk mendeklarasikan jenis tipe data nya
+    var staticVar: Char = 'A'
+    println("Contoh static variabel" + staticVar)
+}
