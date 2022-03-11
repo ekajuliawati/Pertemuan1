3c4eea3a7a54a37d7056ec0aa74645ccea0175af
Eka Juliawati <ekajuliawati63@gmail.com>
2022-03-11 16:39:31 +0700

    percobaan var dan val

--- src/main/kotlin/5_var dan val.kt
+++ src/main/kotlin/5_var dan val.kt
@@ -8,7 +8,7 @@ fun main() {
 
     // val bernilai tetap
     val phi = 3.14
-    phi = 2.5
+
     // Ketika kita merubah nilai variabel val phi untuk kedua kalinya maka akan terdapat pesan
     //kesalahan agar kita menggunakan var variabel.
 }
