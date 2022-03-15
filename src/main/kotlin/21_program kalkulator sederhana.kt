b608eccd7598145676cf8b7bef3f8960e54e5a00
Eka Juliawati <ekajuliawati63@gmail.com>
2022-03-15 22:22:08 +0700

    Program kaltulator sederhana

---
+++ src/main/kotlin/21_program kalkulator sederhana.kt
@@ -0,0 +1,18 @@
+// Program Kalkulator Sederhana
+fun main(args: Array<String>) {
+//Program ini menerima 2 masukkan bilangan dan akan melakukan operasi tambah, kurang, kali dan bagi dari kedua bilangan yang dimasukkan.
+    println("Welcome to calculator")
+    println("Enter the first number:")
+    val a = readLine()!!.toDouble()
+    println("Enter the second number:")
+    val b = readLine()!!.toDouble()
+    val sum = a + b
+    val difference = a - b
+    val product = a * b
+    val quotient = a / b
+    println("Sum: ${sum}")
+    println("Difference: ${difference}")
+    println("Product: ${product}")
+    println("Quotient: ${quotient}")
+    println("Thank you for using calculator.")
+}
